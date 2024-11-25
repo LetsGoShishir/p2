23de94e055d934778573d0d7c6c6bb954160c1f6
LetsGoShishir <shishirshishir243@gmail.com>
2024-11-25 16:41:49 +0600

    commit

--- pj-2/src/App.jsx
+++ pj-2/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation.jsx"
+import ContactHeader from "./components/ContactHeader/ContactHeader.jsx"
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+
 
 function App() {
   return (
     <div>
       <Navigation/>
+      <ContactHeader/>
+      <ContactForm/>
+     
       
     </div>
   );
---
+++ pj-2/src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./ContactHeader.module.css"
+
+
+const ContactHeader = () => {
+  return (
+
+    <div className={`${styles.ContactHeader} container`}>
+      <h1>Contact Us</h1>
+      <p>here we are ,make the things more </p>
+    </div>
+  )
+}
+
+export default ContactHeader
