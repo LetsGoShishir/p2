202c96051b97c0efe45e8e94ca3cd3c942023410
LetsGoShishir <shishirshishir243@gmail.com>
2024-11-25 21:54:37 +0600

    commit

--- pj-2/src/App.jsx
+++ pj-2/src/App.jsx
@@ -1,19 +1,19 @@
-import "./App.css";
-import Navigation from "./components/Navigation/Navigation.jsx"
-import ContactHeader from "./components/ContactHeader/ContactHeader.jsx"
-import ContactForm from "./components/ContactForm/ContactForm.jsx";
 
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
-      <Navigation/>
-      <ContactHeader/>
-      <ContactForm/>
-     
-      
+      <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
     </div>
   );
-};
+}
 
 export default App;
